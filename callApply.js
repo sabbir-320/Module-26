b9472bed6a27030b5b92320d96a8abd697a3d9ec
sabbir-320 <sabbir195661@gmail.com>
2020-08-10 15:38:38 +0600

    difference between bind, call and apply

--- callApply.js
+++ callApply.js
@@ -4,8 +4,8 @@ const richPerson = {
     firstName: "Ali",
     lastName: "Omor",
     salary: 20000,
-    bill: function (amount){
-     balance = this.salary - amount;
+    bill: function (amount, tips){
+     balance = this.salary - (amount + tips);
       return balance;
     }
 }
@@ -19,6 +19,25 @@ const poorPerson = {
 }
 const poorAmount = richPerson.bill.bind(poorPerson);
 console.log(poorAmount(500));
+
+// an object uses method from another object directly and more then argument separate by comma
+const honestPerson = {
+    firstName: "Abdul",
+    lastName: "Hakim",
+    salary: 50000
+}
+const honestPersonAmount = richPerson.bill.call(honestPerson, 10000, 20000);
+console.log(honestPersonAmount);
+
+// an object uses method from another object directly and more then argument separate by array
+
+const badPerson = {
+    firstName: "Micel",
+    lastName: "Dark",
+    salary: 200000
+}
+const badPersonAmount = richPerson.bill.apply(badPerson, [100000, 50000])
+console.log(badPersonAmount);
 
 
 
